df44303c4aada1567637491e187b9a02ec7c3b31
axlecho <axlecho@gmail.com>
2019-08-24 03:06:20 +0800

    [feature] update document and test resource

---
+++ api/src/test/resources/MHViewer Pingin.md
@@ -0,0 +1,80 @@
+MHViewer Pingin
+===
+
+info
+---
+
+```json
+ {
+ "info": {
+    "gid": "208146",
+    "title": "五等分的新娘",
+    "titleJpn": "五等分の花嫁",
+    "thumb": "http:\/\/lain.bgm.tv\/pic\/cover\/c\/51\/85\/208146_E98lC.jpg",
+    "category": -1,
+    "posted": "",
+    "uploader": "春场葱",
+    "rating": 7.3,
+    "rated": true,
+    "source": "Bangumi"
+  },
+  "intro": "　　一直過著貧窮生活的資優生上杉風太郎，有一天居然得到了一個從天而降的大好打工機會，那就是當有錢人家女兒的家教。可是對象居然是自己曾經得罪過的女同學！不只如此，風太郎還發現了一個驚人的事實，他的學生不是一個人，而是如假包換的五胞胎！面對超有個性、瀕臨留級卻抗拒唸書的五胞胎，風太郎有辦法完成任務，讓她們五人順利畢業嗎？",
+  "chapterCount": 0,
+  "favoriteCount": 714,
+  "isFavorited": false,
+  "ratingCount": 316,
+  "chapters": [{
+      "title": "1话",
+      "url": "178538",
+      "source": "Manhuadui"
+    },  {
+      "title": "2话",
+      "url": "178539",
+      "source": "Manhuadui"
+    }],
+  "comments": [
+    {
+      "id": "481595",
+      "score": 0,
+      "time": "2d 18h ago",
+      "user": "TeammateBane",
+      "comment": "希望最终结局是一个好结局吧。除了最终获得胜利的那位，其他四人都可以找到自己真正想要的。",
+      "source": "Bangumi"
+    }],
+  "source": "Bangumi",
+  "updateTime": 0
+} 
+``` 
+
+list
+---
+```json
+{
+   "datas": [
+     {  "gid": "23270",
+       "title": "Dr.STONE",
+       "titleJpn": "",
+       "thumb": "",
+       "category": 0,
+       "posted": "2019-08-17",
+       "uploader": "稻垣理一郎,Boichi",
+       "rating": 9.4,
+       "rated": false,
+       "source": "Manhuagui"
+     }],
+   "pages": 1,
+   "currentPage": 1
+ }
+ ```
+
+data
+---
+```json
+{
+ 	"data": [
+    	"https:\/\/i.hamreus.com\/ps3\/h\/hydxjxrwgb\/第02回\/P035.jpg.webp?cid=183592&md5=1FjDMlNVXJKTULGyLOH5ow",
+    	"https:\/\/i.hamreus.com\/ps3\/h\/hydxjxrwgb\/第02回\/zhaorenV1.jpg.webp?cid=183592&md5=1FjDMlNVXJKTULGyLOH5ow"
+  	],
+  "source": "Manhuagui"
+}
+```
--- api/src/test/resources/parser.js
+++ api/src/test/resources/parser.js
@@ -1,87 +1,61 @@
 function parseTitle() {
-    return jsoup.text("ul.ar_list_coc li:eq(0)");
+    return jsoup.text('ul.ar_list_coc li:eq(0)') + '';
 };
 
-function data() {
+function DomNode(node) {
+    this.text = function(cssQuery) { return (cssQuery===undefined ? node.text() : node.text(cssQuery)) + ''};
+    this.src  = function(cssQuery) { return (cssQuery===undefined ? node.src() : node.src(cssQuery)) + ''};
+    this.attr = function(attr,cssQuery) { return (cssQuery===undefined ? node.attr(attr) : node.attr(cssQuery,attr)) + ''};
+    this.herf = function(cssQuery) { return (cssQuery===undefined ? node.herf() : node.herf(cssQuery)) + ''};
+    // Warning this function return a java object
+    this.list = function(cssQuery) { 
+        var result = [];
+        for (var i = 0; i < node.list(cssQuery); i++) {
+            result.push(new DomNode(node.list(cssQuery).get(i)));
+        }
+        return result;
+    };
+}
 
-};
+var doc = new DomNode(jsoup)
 
-// {
-// "info": {
-//    "gid": "208146",
-//    "title": "五等分的新娘",
-//    "titleJpn": "五等分の花嫁",
-//    "thumb": "http:\/\/lain.bgm.tv\/pic\/cover\/c\/51\/85\/208146_E98lC.jpg",
-//    "category": -1,
-//    "posted": "",
-//    "uploader": "春场葱",
-//    "rating": 7.3,
-//    "rated": true,
-//    "source": "Bangumi"
-//  },
-//  "intro": "　　一直過著貧窮生活的資優生上杉風太郎，有一天居然得到了一個從天而降的大好打工機會，那就是當有錢人家女兒的家教。可是對象居然是自己曾經得罪過的女同學！不只如此，風太郎還發現了一個驚人的事實，他的學生不是一個人，而是如假包換的五胞胎！面對超有個性、瀕臨留級卻抗拒唸書的五胞胎，風太郎有辦法完成任務，讓她們五人順利畢業嗎？",
-//  "chapterCount": 0,
-//  "favoriteCount": 714,
-//  "isFavorited": false,
-//  "ratingCount": 316,
-//  "chapters": [],
-//  "comments": [
-//    {
-//      "id": "481595",
-//      "score": 0,
-//      "time": "2d 18h ago",
-//      "user": "TeammateBane",
-//      "comment": "希望最终结局是一个好结局吧。除了最终获得胜利的那位，其他四人都可以找到自己真正想要的。",
-//      "source": "Bangumi"
-//    }],
-//  "source": "Bangumi",
-//  "updateTime": 0
-//}
 function info(gid) {
     var result = {};
     result.info = {};
-    result.info.gid = gid;
-    result.info.title = $("ul.ar_list_coc li:eq(0)",doc).text();
-    result.info.titleJpn = "";
-    result.info.posted = $("ul.ar_list_coc li:eq(4)",doc).text().replace(/[^0-9]/ig,"");
-    result.info.thumb = $("div.ar_list_coc img",doc).attr("src");
-    result.info.uploader =  $("ul.ar_list_coc li:eq(1) a",doc).text();
-    result.intro = $("i#det",doc).text();
-    result.chapterCount = $("ul.ar_list_coc li:eq(3)",doc).text().replace(/[^0-9]/ig,"");
+    result.info.gid = gid + '';
+    result.info.title =  doc.text('ul.ar_list_coc li:eq(0)');
+    result.info.titleJpn = '';
+    result.info.posted = doc.text('ul.ar_list_coc li:eq(4)').replace(/[^0-9]/ig,'');
+    result.info.thumb = doc.src('div.ar_list_coc img');
+    result.info.uploader =  doc.text('ul.ar_list_coc li:eq(1) a'); 
+    result.intro = doc.text('i#det') + '';
+    result.chapterCount = doc.text('ul.ar_list_coc li:eq(3)').replace(/[^0-9]/ig,'');
     result.favoriteCount = 0;
     result.chapters = [];
+
+    doc.list('ul.ar_rlos_bor li').forEach(function(i,v) {
+        var chapter = {};
+        chapter.title = v.text('a');
+        chapter.url = node.href('a');
+        result.chapters.push(chapter);
+    });
+    
     result.comments = [];
     updateTime = 0;
     
-    console.log(result);
+    // console.log(result);
     return JSON.stringify(result);
 };
 
-//{
-//   "datas": [
-//     {  "gid": "23270",
-//       "title": "Dr.STONE",
-//       "titleJpn": "",
-//       "thumb": "",
-//       "category": 0,
-//       "posted": "2019-08-17",
-//       "uploader": "稻垣理一郎,Boichi",
-//       "rating": 9.4,
-//       "rated": false,
-//       "source": "Manhuagui"
-//     }],
-//   "pages": 1,
-//   "currentPage": 1
-// }
 function recent() {
-     var result = {};
+    var result = {};
     result.datas = [];
-    $("ul.new_hits_ul li",doc).each(function(i,n){
+    $('ul.new_hits_ul li').each(function(i,n){
         var item = {};
-        item.gid = $("a",n).attr("href").replace(/[^0-9]/ig,"");
-        item.title = $("a",n).text();
-        item.titleJpn = "";
-        item.thumb = $("img",n).attr("src");
+        item.gid = $('a',n).attr('href').replace(/[^0-9]/ig,'');
+        item.title = $('a',n).text();
+        item.titleJpn = '';
+        item.thumb = $('img',n).attr('src');
         item.category = 0;
         console.log(item);
         result.datas.push(item);
@@ -96,12 +70,12 @@ function recent() {
 function _top() {
     var result = {};
     result.datas = [];
-    $("ul.new_hits_ul li",doc).each(function(i,n){
+    $('ul.new_hits_ul li').each(function(i,n){
         var item = {};
-        item.gid = $("a",n).attr("href").replace(/[^0-9]/ig,"");
-        item.title = $("a",n).text();
-        item.titleJpn = "";
-        item.thumb = $("img",n).attr("src");
+        item.gid = $('a',n).attr('href').replace(/[^0-9]/ig,'');
+        item.title = $('a',n).text();
+        item.titleJpn = '';
+        item.thumb = $('img',n).attr('src');
         item.category = 0;
         console.log(item);
         result.datas.push(item);
@@ -116,36 +90,27 @@ function _top() {
 function search() {
     var result = {};
     result.datas = [];
-    $("div.ar_list_co ul dl",doc).each(function(i,n){
+    $('div.ar_list_co ul dl').each(function(i,n){
         var item = {};
-        item.gid = $("a",n).attr("href").replace("https://www.177mh.net","").replace(/[^0-9]/ig,"");
-        item.title = $("h1",n).text();
-        item.titleJpn = "";
-        item.thumb = $("img",n).attr("src");
+        item.gid = $('a',n).attr('href').replace('https://www.177mh.net','').replace(/[^0-9]/ig,'');
+        item.title = $('h1',n).text();
+        item.titleJpn = '';
+        item.thumb = $('img',n).attr('src');
         item.category = 0;
-        item.uploader = $("i.author a:eq(1)",n).text()
+        item.uploader = $('i.author a:eq(1)',n).text()
         console.log(item);
         result.datas.push(item);
     });
-    result.pages = $("div.pages_s",doc).text().split("/")[1].replace(/[^0-9]/ig,"");
-    result.currentPage = $("div.pages_s",doc).text().split("/")[0].replace(/[^0-9]/ig,"");
-    
+    result.pages = $('div.pages_s').text().split('/')[1].replace(/[^0-9]/ig,'');
+    result.currentPage = $('div.pages_s').text().split('/')[0].replace(/[^0-9]/ig,'');
+
     console.log(result);   
     return JSON.stringify(result);   
 };
 
-
- // {
-// "data": [
-//    "https:\/\/i.hamreus.com\/ps3\/h\/hydxjxrwgb\/第02回\/P035.jpg.webp?cid=183592&md5=1FjDMlNVXJKTULGyLOH5ow",
-//    "https:\/\/i.hamreus.com\/ps3\/h\/hydxjxrwgb\/第02回\/zhaorenV1.jpg.webp?cid=183592&md5=1FjDMlNVXJKTULGyLOH5ow"
-//  ],
-//  "source": "Manhuagui"
-// }
-
 function data() {
     var result = {};
-    result.data = msg.split("|");
+    result.data = msg.split('|');
     
     console.log(result);   
     return JSON.stringify(result);     
