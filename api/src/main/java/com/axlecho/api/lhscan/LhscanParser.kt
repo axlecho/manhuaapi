c0bb8fe31248b20d764937d127e700862f063d1b
axlecho <axlecho@gmail.com>
2019-12-09 22:42:22 +0800

    [feature][lhscan] update parser

--- api/src/main/java/com/axlecho/api/lhscan/LhscanParser.kt
+++ api/src/main/java/com/axlecho/api/lhscan/LhscanParser.kt
@@ -2,7 +2,6 @@ package com.axlecho.api.lhscan
 
 import com.axlecho.api.*
 import com.axlecho.api.untils.MHNode
-import com.axlecho.api.untils.MHStringUtils
 import com.orhanobut.logger.Logger
 
 class LhscanParser {
@@ -10,13 +9,16 @@ class LhscanParser {
         private val Tag: String = LhscanParser::javaClass.name
 
         fun String.filterDigital(): String {
-            return this.replace("[^0-9]".toRegex(), "");
+            return this.replace("[^0-9]".toRegex(), "")
         }
 
-        fun String.filterEnd(): String {
+        private fun String.filterEnd(): String {
             return this.replace(".html", "")
         }
 
+        private fun String.filterChapter(): String {
+            return this.substringAfter("Chapter")
+        }
 
         fun parserComicList(html: String): MHMutiItemResult<MHComicInfo> {
             Logger.v(html)
@@ -41,26 +43,24 @@ class LhscanParser {
         fun parserInfo(html: String, gid: String): MHComicDetail {
             Logger.v(html)
             val body = MHNode(html)
-
             val title = body.text("ul.manga-info > h1").replace(" - RAW", "")
             val titleJpn = MHConstant.UNKNOWN_TITLE
             val thumb = body.src("img.thumbnail")
             val category = -1
             val posted = MHConstant.UNKNOWN_TIME
             val updateTime = 0L
-            val uploader = body.text("ul.manga-info > h1")
+            val uploader = body.text("ul.manga-info > li:eq(3) > small")
             val rating = 0.0f
             val ratingCount = 0
             val rated = ratingCount > 0
             val info = MHComicInfo(gid, title, titleJpn, thumb, category, posted, uploader, rating, rated, MHApiSource.Lhscan)
 
             val intro = body.text("h3:contains(Description)")
-            val favoriteCount = 0
+            val favoriteCount =  body.text("ul.manga-info > li:eq(7)").filterDigital().toInt()
             val isFavorited = false
             val chapters = ArrayList<MHComicChapter>()
-            val name = body.text("#about_kit > ul > li:eq(0) > h1")
             for (node in body.list("div#tab-chapper > div > ul > table > tbody > tr")) {
-                val chapterTitle = node.text("a.chapter")
+                val chapterTitle = node.text("a.chapter").filterChapter()
                 val path = node.href("a.chapter").filterEnd()
                 chapters.add(MHComicChapter(chapterTitle.trim { it <= ' ' }, path, MHApiSource.Lhscan))
             }
@@ -77,17 +77,6 @@ class LhscanParser {
                 list.add(node.src())
             }
             return MHComicData(list, MHApiSource.Lhscan)
-        }
-
-        fun parserRaw(html: String): String {
-            Logger.v(html)
-            val body = MHNode(html)
-            val script = body.list("td[valign*=top] > script").first().get().html()
-            Logger.v(script)
-
-            val url = MHStringUtils.match("<IMG SRC='(.*?)'>", script, 1)
-
-            return url.replace(Regex("\".*\""), "http://n9.1whour.com/")
         }
     }
 }
